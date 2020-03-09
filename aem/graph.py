4d6638607cbf389f27aec99c82229ddaa5cc3d57
damian-horna <damian.horna@interia.pl>
2020-03-09 16:22:45 +0100

    Started work on greedy

--- aem/graph.py
+++ aem/graph.py
@@ -1,4 +1,12 @@
+import numpy as np
+
+
 class Graph:
 
     def __init__(self, adjacency_matrix):
         self.adjacency_matrix = adjacency_matrix
+
+    def get_closest_neighbor_index(self, vertex_no):
+        valid_idx = np.where(self.adjacency_matrix[vertex_no, :] != 0)[0]
+        out = valid_idx[self.adjacency_matrix[vertex_no, :][valid_idx].argmin()]
+        return out
--- aem/greedy.py
+++ aem/greedy.py
@@ -6,5 +6,15 @@ class Greedy:
         self.graph = graph
 
     def run(self) -> ExperimentResult:
+        cycle = []
+        start_vertex = 0
+        cycle.append(start_vertex)
+        closest_neighbor_idx = self.graph.get_closest_neighbor_index(start_vertex)
+        cycle.append(closest_neighbor_idx)
+
+        # now expand the cycle as long as len(cycle) < 50
+
+        
+
         return ExperimentResult(0, 0, 0)
 
