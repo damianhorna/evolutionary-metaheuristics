c34ae34467a79d112f4365b9a2120ad7636f1d8f
damian-horna <damian.horna@interia.pl>
2020-03-20 15:21:02 +0100

    K-regret done, add saving results to csv and latex

--- aem/heuristics/k_regret.py
+++ aem/heuristics/k_regret.py
@@ -10,36 +10,37 @@ class KRegret(Heuristic):
         cycle = [start_vertex]
         next_vertex = self.graph.get_closest_unvisited_neighbor_idx(from_vertex=cycle[-1], visited=cycle)
         cycle.append(next_vertex)
-        while len(cycle) < 50:
-            # we can insert either first or second closest depending on cost (1-regret)
-            idx_1, cost_1 = self.select_greedy(cycle)
-            tmp_cycle = cycle + [idx_1]
-            idx_2, cost_2 = self.select_greedy(tmp_cycle)
-            total_cost_1_2 = cost_1 + cost_2
-
-            idx_3, cost_3 = self.select_greedy(cycle, exclude=[idx_1])
-            tmp_cycle = cycle + [idx_3]
-            idx_4, cost_4 = self.select_greedy(tmp_cycle)
-            total_cost_3_4 = cost_3 + cost_4
-
-            if total_cost_1_2 < total_cost_3_4:
-                cycle.append(idx_1)
-            else:
-                cycle.append(idx_3)
-
+        while len(cycle) < self.graph.no_of_vertices() // 2:
+            costs_dict = self.compute_insertion_costs(cycle)
+            costs_dict = self.sort_insertion_costs(costs_dict)
+            k_regret_dict = self.compute_k_regret_dict(costs_dict)
+            best_candidate_to_insert = max(k_regret_dict, key=k_regret_dict.get)
+            best_insert_position, best_insert_cost = costs_dict[best_candidate_to_insert][0]
+            cycle.insert(best_insert_position, best_candidate_to_insert)
         return cycle
 
-    def select_greedy(self, cycle, exclude=[]):
-        min_cost = float('inf')
-        min_cost_vertex_idx = None
-        for i in range(self.graph.no_of_vertices()):
-            if i not in cycle and i not in exclude:
-                first_closest = self.graph.get_closest_visited_idx(from_vertex=i, visited=cycle)
-                tmp_cycle = [i for i in cycle if i != first_closest]
-                second_closest = self.graph.get_closest_visited_idx(from_vertex=i, visited=tmp_cycle)
+    def compute_k_regret_dict(self, costs_dict):
+        k_regret_dict = {}
+        for vertex in costs_dict:
+            k_regret_dict[vertex] = 0
+            for i in range(self.k):
+                k_regret_dict[vertex] += costs_dict[vertex][i+1][1] - costs_dict[vertex][0][1]
+        return k_regret_dict
 
-                insertion_cost = self.graph.cost(first_closest, i) + self.graph.cost(second_closest, i)
-                if insertion_cost < min_cost:
-                    min_cost = insertion_cost
-                    min_cost_vertex_idx = i
-        return min_cost_vertex_idx, min_cost
+    def compute_insertion_costs(self, cycle: list) -> dict:
+        costs = {}
+        for vertex in range(self.graph.no_of_vertices()):
+            if vertex not in cycle:
+                costs[vertex] = []
+                for position in range(len(cycle)):
+                    tmp_cycle = cycle.copy()
+                    tmp_cycle.insert(position, vertex)
+                    cycle_cost = self.graph.compute_cycle_length(tmp_cycle)
+                    costs[vertex].append((position, cycle_cost))
+        return costs
+
+    @staticmethod
+    def sort_insertion_costs(costs: dict) -> dict:
+        for vertex in costs:
+            costs[vertex] = sorted(costs[vertex], key=lambda x: x[1])
+        return costs
--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from aem.heuristics.nearest_neighbor import NearestNeighbor
 from aem.heuristics.greedy import Greedy
 from aem.heuristics.k_regret import KRegret
 from aem.utils.plot_util import PlotUtil
+from pandas import DataFrame
 
 instances = [
     "../data/kroA100.tsp",
     "../data/kroB100.tsp"
 ]
 
+results = {}
+
 for instance in instances:
     print(f"Working on {instance}")
     graph = TSPReader().read_graph_with_coords(instance)
-    methods = [NearestNeighbor(graph), Greedy(graph)]  # KRegret(graph)
+    methods = [NearestNeighbor(graph), Greedy(graph), KRegret(graph, k=1)]
     for method in methods:
         result = method.run()
+        results[result.method_classname] = [result.average, result.min, result.max]
         result.print()
         PlotUtil.plot_best_cycle(result, graph.coords, instance)
+
+df = DataFrame.from_dict(results)
+df.to_csv("results.csv")
+with open("result.latex", "w") as f:
+    f.write(df.to_latex())
