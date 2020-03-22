6805ba3fd33ff2cad2b5c25ca4f75016bbc5f86f
damian-horna <damian.horna@interia.pl>
2020-03-22 18:50:03 +0100

    Replot

--- aem/utils/plot_util.py
+++ aem/utils/plot_util.py
@@ -12,9 +12,9 @@ class PlotUtil:
         all_x, all_y = zip(*list(coords.values()))
         plt.figure()
         plt.title(f"{res.method_classname} best cycle ({instance[8:-4]})")
-        plt.scatter(all_x, all_y, s=10)
+        plt.scatter(all_x, all_y, s=25)
         for i, (x,y) in enumerate(zip(all_x, all_y)):
-            plt.annotate(f"{i+1}", (x,y), textcoords="offset points", xytext=(0,4), ha="center", fontsize=6)
+            plt.annotate(f"{i+1}", (x,y), textcoords="offset points", xytext=(0,4), ha="center", fontsize=9)
         plt.plot(cycle_coords_x + [cycle_coords_x[0]], cycle_coords_y + [cycle_coords_y[0]], c='r')
         plt.savefig(f"plots/{res.method_classname}-{instance[8:-4]}-best.png")
         plt.close()
