adcacc420542c54f12b6ddc1000d7d834dd87586
Lukasz Eckert <lukasz.eckert@deep.bi>
2020-03-22 17:45:44 +0100

    Change: numpy instead of loops

--- aem/heuristics/k_regret.py
+++ aem/heuristics/k_regret.py
@@ -1,4 +1,5 @@
 from aem.heuristics.heuristic import Heuristic
+import numpy as np
 
 
 class KRegret(Heuristic):
@@ -10,37 +11,60 @@ class KRegret(Heuristic):
         cycle = [start_vertex]
         next_vertex = self.graph.get_closest_unvisited_neighbor_idx(from_vertex=cycle[-1], visited=cycle)
         cycle.append(next_vertex)
-        while len(cycle) < self.graph.no_of_vertices() // 2:
-            costs_dict = self.compute_insertion_costs(cycle)
-            costs_dict = self.sort_insertion_costs(costs_dict)
-            k_regret_dict = self.compute_k_regret_dict(costs_dict)
-            best_candidate_to_insert = max(k_regret_dict, key=k_regret_dict.get)
-            best_insert_position, best_insert_cost = costs_dict[best_candidate_to_insert][0]
-            cycle.insert(best_insert_position, best_candidate_to_insert)
+
+        available = [x for x in range(self.graph.no_of_vertices()) if x not in cycle]
+        excepted_no_of_vertices = self.graph.no_of_vertices() // 2
+        while len(cycle) < excepted_no_of_vertices:
+            costs = self.compute_insertion_costs(cycle, available)
+
+            costs_sorted_idx = costs.argsort(axis=1)
+            costs_sorted = costs.copy()
+            costs_sorted.sort(axis=1)
+
+            poz = np.argmax(np.sum(costs_sorted[:, 1:self.k + 1], axis=1) - costs_sorted[:, 0])
+
+            cycle.insert(costs_sorted_idx[poz][0], available[poz])
+            available.pop(poz)
+
         return cycle
 
-    def compute_k_regret_dict(self, costs_dict):
-        k_regret_dict = {}
-        for vertex in costs_dict:
-            k_regret_dict[vertex] = 0
-            for i in range(self.k):
-                k_regret_dict[vertex] += costs_dict[vertex][i+1][1] - costs_dict[vertex][0][1]
-        return k_regret_dict
+    def compute_insertion_costs(self, cycle: list, available: list) -> dict:
+        roll = [cycle[-1]] + cycle[:-1]  # np.roll(cycle, 1)
+        res = self.graph.adjacency_matrix[cycle][:, available].T + self.graph.adjacency_matrix[available][:, roll] - \
+              self.graph.adjacency_matrix[cycle, roll]
+        return res
 
-    def compute_insertion_costs(self, cycle: list) -> dict:
-        costs = {}
-        for vertex in range(self.graph.no_of_vertices()):
-            if vertex not in cycle:
-                costs[vertex] = []
-                for position in range(len(cycle)):
-                    tmp_cycle = cycle.copy()
-                    tmp_cycle.insert(position, vertex)
-                    cycle_cost = self.graph.compute_cycle_length(tmp_cycle)
-                    costs[vertex].append((position, cycle_cost))
-        return costs
 
-    @staticmethod
-    def sort_insertion_costs(costs: dict) -> dict:
-        for vertex in costs:
-            costs[vertex] = sorted(costs[vertex], key=lambda x: x[1])
-        return costs
+class KRegretFilter(Heuristic):
+    def __init__(self, graph, k=1):
+        super().__init__(graph)
+        self.k = k
+
+    def build_cycle(self, start_vertex=0) -> list:
+        cycle = [start_vertex]
+        next_vertex = self.graph.get_closest_unvisited_neighbor_idx(from_vertex=cycle[-1], visited=cycle)
+        cycle.append(next_vertex)
+
+        available = [x for x in range(self.graph.no_of_vertices()) if x not in cycle]
+        excepted_no_of_vertices = self.graph.no_of_vertices() // 2
+        while len(cycle) < excepted_no_of_vertices:
+            costs = self.compute_insertion_costs(cycle, available)
+            best_idx = np.argsort(np.min(costs, axis=1))[:excepted_no_of_vertices - len(cycle)]
+
+            costs = costs[best_idx]
+            costs_sorted_idx = costs.argsort(axis=1)
+            costs_sorted = costs.copy()
+            costs_sorted.sort(axis=1)
+
+            poz = np.argmax(np.sum(costs_sorted[:, 1:self.k + 1], axis=1) - costs_sorted[:, 0])
+
+            cycle.insert(costs_sorted_idx[poz][0], available[best_idx[poz]])
+            available.pop(best_idx[poz])
+
+        return cycle
+
+    def compute_insertion_costs(self, cycle: list, available: list) -> dict:
+        roll = [cycle[-1]] + cycle[:-1]  # np.roll(cycle, 1)
+        res = self.graph.adjacency_matrix[cycle][:, available].T + self.graph.adjacency_matrix[available][:, roll] - \
+              self.graph.adjacency_matrix[cycle, roll]
+        return res
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from aem.utils.tsp_reader import TSPReader
 from aem.heuristics.nearest_neighbor import NearestNeighbor
 from aem.heuristics.greedy import Greedy
-from aem.heuristics.k_regret import KRegret
+from aem.heuristics.k_regret import KRegret, KRegretFilter
 from aem.utils.plot_util import PlotUtil
 from pandas import DataFrame
 
 instances = [
-    "../data/kroA100.tsp",
-    "../data/kroB100.tsp"
+    "data/kroA100.tsp",
+    "data/kroB100.tsp"
 ]
 
 results = {}
@@ -15,7 +15,7 @@ results = {}
 for instance in instances:
     print(f"Working on {instance}")
     graph = TSPReader().read_graph_with_coords(instance)
-    methods = [NearestNeighbor(graph), Greedy(graph), KRegret(graph, k=1)]
+    methods = [NearestNeighbor(graph), Greedy(graph), KRegret(graph, k=1), KRegretFilter(graph, k=1)]
     for method in methods:
         result = method.run()
         results[result.method_classname] = [result.average, result.min, result.max]
