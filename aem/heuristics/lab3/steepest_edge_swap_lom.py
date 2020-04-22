80c47ef6a05c24df94d2389426221ba685aef97b
damian-horna <damian.horna@interia.pl>
2020-04-23 00:24:12 +0200

    Adding new moves

--- aem/heuristics/lab3/steepest_edge_swap_lom.py
+++ aem/heuristics/lab3/steepest_edge_swap_lom.py
@@ -1,3 +1,4 @@
+from aem.heuristics.lab3.moves import EdgeSwap, NodeSwap
 from aem.heuristics.lab3.steepest_heuristic import SteepestHeuristic
 
 
@@ -9,11 +10,11 @@ class SteepestEdgeSwapListOfMoves(SteepestHeuristic):
         self.LM_lookup = set()
 
     def alter_cycle(self, cycle):
-        if self.new_moves is None: # first iteration
+        if self.new_moves is None:  # first iteration
             self.new_moves = self.all_moves(cycle)
-        else:
-            # TODO: how to generate new moves smarter?
-            self.new_moves = [move for move in self.all_moves(cycle) if move not in self.LM_lookup]
+        # else:
+        #     # TODO: how to generate new moves smarter?
+        #     self.new_moves = [move for move in self.all_moves(cycle) if move not in self.LM_lookup]
         for move in self.new_moves:
             delta = move.get_delta(self.graph)
             move.delta = delta
@@ -26,6 +27,37 @@ class SteepestEdgeSwapListOfMoves(SteepestHeuristic):
 
         if len(self.LM) > 0:
             best_move = self.LM[0]
-            return best_move.alter(cycle), True
+            altered_cycle = best_move.alter(cycle)
+            self.add_new_moves(best_move, cycle)
+            return altered_cycle, True
         else:
             return cycle, False
+
+    def add_new_moves(self, best_move, cycle):
+        self.new_moves = []
+        if isinstance(best_move, EdgeSwap):
+            added_edge1 = (best_move.succ_n2, best_move.succ_n1)
+            added_edge2 = (best_move.n2, best_move.n1)
+            edges = []
+            for i in range(len(cycle)):
+                edges.append((cycle[i], cycle[(i + 1) % len(cycle)]))
+
+            for edge in edges:
+                self.new_moves.append(EdgeSwap(added_edge1, edge))
+                self.new_moves.append(EdgeSwap(added_edge2, edge))
+        elif isinstance(best_move, NodeSwap):
+            new_inner_pos = cycle.index(best_move.outer)
+            not_in_cycle = set(range(self.graph.no_of_vertices())) - set(cycle)
+            for v in not_in_cycle:
+                new_node_swap = NodeSwap(best_move.outer,
+                                         v,
+                                         (cycle[new_inner_pos - 1], cycle[(new_inner_pos + 1) % len(cycle)]))
+                self.new_moves.append(new_node_swap)
+            edges = []
+            for i in range(len(cycle)):
+                edges.append((cycle[i], cycle[(i + 1) % len(cycle)]))
+            added_edge1 = (best_move.inner_neighbors[0], best_move.outer)
+            added_edge2 = (best_move.outer, best_move.inner_neighbors[1])
+            for edge in edges:
+                self.new_moves.append(EdgeSwap(added_edge1, edge))
+                self.new_moves.append(EdgeSwap(added_edge2, edge))
