417c37aa4244aecb2b5445cd2ddb90fc3333f98f
damian-horna <damian.horna@interia.pl>
2020-05-17 14:33:38 +0200

    Started working on hybrid evolutionary

---
+++ main_lab5.py
@@ -0,0 +1,33 @@
+from pandas import DataFrame
+
+from aem.heuristics.lab3.steepest_edge_swap import SteepestEdgeSwap
+from aem.heuristics.lab3.steepest_edge_swap_candidate import SteepestEdgeSwapCandidate
+from aem.heuristics.lab3.steepest_edge_swap_lom import SteepestEdgeSwapListOfMoves
+from aem.heuristics.lab4.MultipleStartLocalSearch import MultipleStartLocalSearch
+from aem.heuristics.lab5.IterateLocalSearchPeturbiation import HybridEvolutionary
+from aem.utils.plot_util import PlotUtil
+from aem.utils.tsp_reader import TSPReader
+
+instances = [
+    "data/kroA200.tsp",
+    "data/kroB200.tsp"
+]
+
+results = {}
+#
+for instance in instances:
+    print(f"Working on {instance}")
+    graph = TSPReader().read_graph_with_coords(instance)
+
+    methods = [HybridEvolutionary(graph)]
+    for method in methods:
+        result = method.run(seed=13, time_limit=33.5, population_size=20, number_of_experiments=1)
+        results[result.method_classname] = [result.average, result.min, result.max, result.time_average,
+                                            result.time_min, result.time_max]
+        result.print()
+        PlotUtil.plot_best_cycle(result, graph.coords, instance)
+
+    df = DataFrame.from_dict(results)
+    df.to_csv(f"results-{instance[8:-4]}.csv")
+    with open(f"result-{instance[8:-4]}.latex", "w") as f:
+        f.write(df.to_latex())
